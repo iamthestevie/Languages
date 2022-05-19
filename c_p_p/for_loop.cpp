03eb0aa4e420a02a98b52cae4543242c8f0c3937
Steve Muir <stephen.l.muir@gmail.com>
2022-05-19 13:52:40 -0700

    HackerRank C++ - 5_for_loop.cpp

---
+++ c_p_p/for_loop.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // for loops
+
+    // syntax
+    // for ( <expression_1> ; <expression_2> ; <expression_3> )
+    //     <statement>
+
+    // expression_1 is used for intializing variables which are generally used for controlling the terminating flag for the loop.
+    // expression_2 is used to check for the terminating condition. If this evaluates to false, then the loop is terminated.
+    // expression_3 is generally used to update the flags/variables.
+
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    for (int i = a; i <= b; i++)
+    {
+        if (i > 9)
+        {
+            if (i % 2 == 0) 
+            {
+                cout << "even" << endl;
+            } 
+            else {
+                cout << "odd" << endl;
+            }
+        }
+        else if (i == 9) 
+        {
+            cout << "nine" << endl;
+        }
+        else if (i == 8)
+        {
+            cout << "eight" << endl;
+        }
+        else if (i == 7)
+        {
+            cout << "seven" << endl;
+        }
+        else if (i == 6)
+        {
+            cout << "six" << endl;
+        }
+        else if (i == 5)
+        {
+            cout << "five" << endl;
+        }
+        else if (i == 4)
+        {
+            cout << "four" << endl;
+        }
+        else if (i == 3)
+        {
+            cout << "three" << endl;
+        }
+        else if (i == 2)
+        {
+            cout << "two" << endl;
+        }
+        else if (i == 1)
+        {
+            cout << "one" << endl;
+        }  
+    }
+
+    return 0;
+}
